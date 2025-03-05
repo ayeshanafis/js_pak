9078a0a79cbfb0174f57f94d479e9840aec7af17
ayeshanafis <ayeshanafis476@gmail.com>
2025-03-05 15:04:04 +0500

    data tyes

--- 02_basics/02-datatyes.js
+++ 02_basics/02-datatyes.js
@@ -1,3 +1,22 @@
 "use strict"; // treat all js code as newer version 
 
 console.log(3+3)
+
+//let name="ayesha"
+//let age= 18
+//let loggedIn=true
+
+
+// number=> 2 to power 53
+//bigint (agr nmber bara hai tou ye use kary gy) 
+//string = single quote mei bhi use kar sakty hain our double mei bhi ""
+//boolean= true/false
+//null (dattype bhi hai our standalnoe value hai)
+//undefined= abhi value assign nahi hoi hai
+//symbol (uniqness find karny kaylia use karty hain)
+
+
+//object 
+
+console.log(typeof "ayesha");
+
