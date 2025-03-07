edf19dc531366488defcffb0137c0adf830bc6fe
ayeshanafis <ayeshanafis476@gmail.com>
2025-03-07 10:23:21 +0500

    converstionsoperation

---
+++ 02_basics/03-converstionsoperaion.js
@@ -0,0 +1,19 @@
+//let score= "33"
+
+//console.log(typeof score)
+
+//let valueInNumber= Number(score)
+//console.log(typeof valueInNumber);
+
+
+// "33" -> 33
+// "33abc" -> NaN
+// true -> 1 false-> 0
+
+
+let isLoggedIn= 1
+let booleanisLoggedIn= Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+
+
