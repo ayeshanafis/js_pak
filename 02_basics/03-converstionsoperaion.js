9949876d4cecd73a45fdeef6bba14177588f9d57
ayeshanafis <ayeshanafis476@gmail.com>
2025-03-07 10:52:43 +0500

    converstionsoperation

--- 02_basics/03-converstionsoperaion.js
+++ 02_basics/03-converstionsoperaion.js
@@ -11,9 +11,37 @@
 // true -> 1 false-> 0
 
 
-let isLoggedIn= 1
-let booleanisLoggedIn= Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
+//let isLoggedIn= 1
+//let booleanisLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn)
 
 
 
+//let score=33
+//console.log(typeof score)
+
+
+//********************           operations  */
+
+let value= 3
+let negValue= -value
+//console.log(negValue)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**3)
+//console.log(2/2)
+//console.log(2%2)
+
+
+
+//let str1= "hello"
+//let str2= " ayesha"
+//let str3= str1+str2
+//console.log(str3)
+
+
+let gameCounter= 100
+gameCounter++
+console.log(gameCounter)
