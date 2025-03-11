c1d8b4b6a9cc9a813018cbddd4dd4eac967c988a
ayeshanafis <ayeshanafis476@gmail.com>
2025-03-11 10:55:01 +0500

    comparisions

---
+++ 02_basics/comparisions.js
@@ -0,0 +1,22 @@
+//console.log(2>1) its v simple 
+//console.log(2>=1)
+
+//console.log("2" > 1)
+//console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+console.log(null > 0)
+
+// javascript mei nusll "0" mei convert ho jata hai
+
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined < 0)
+
+// undeifend hamesha en opertors kay stah false result hi dyta hai
+
+
+console.log("2" === 2)
+// es mei wo data type bhi check kary ga
