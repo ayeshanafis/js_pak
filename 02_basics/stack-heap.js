c46b495830b845ee2970d81ad6aeb35fedac56d4
ayeshanafis <ayeshanafis476@gmail.com>
2025-03-12 12:09:42 +0500

    stack-heap

---
+++ 02_basics/stack-heap.js
@@ -0,0 +1,23 @@
+let myYoutubename= "ayesha"
+let anotherName = myYoutubename
+
+anotherName="chaiandcode"
+
+
+//console.log(myYoutubename)
+//console.log(anotherName)
+
+// heap.....
+
+let userOne = {
+emailid= "ayesha@hotmail.com"
+upi= "user@abl"
+}
+
+let userTwo= userOne
+
+userTwo.emailid = "ayesha@gmail.com"
+
+console.log(userOne.emailid)
+console.log(userTwo.emailid)
+
