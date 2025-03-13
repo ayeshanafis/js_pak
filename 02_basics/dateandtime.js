0a5ba0ff76d33b13e3d276bad112ea4f877b2bb0
ayeshanafis <ayeshanafis476@gmail.com>
2025-03-13 15:17:43 +0500

    dateandtime

---
+++ 02_basics/dateandtime.js
@@ -0,0 +1,31 @@
+
+// date..................
+
+
+//let myDate = new Date()
+//console.log(myDate)
+
+// es sy jo date aygi wo readable nhiho gi
+
+let myDate = new Date()
+//console.log(myDate.toString())
+
+//console.log(myDate.toDateString())
+
+console.log(myDate.toLocaleString())
+
+console.log(typeof myDate)
+
+// date ki date type object hai js kay ander
+
+
+// to declare specfic date...
+
+//let myCreateDate = new Date(2025, 0, 23)
+
+//let myCreateDate = new Date(2025, 0, 23, 5, 3)
+
+let myCreateDate = new Date("2025-01-14")
+console.log(myCreateDate.toLocaleString())
+
+
