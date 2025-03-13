73ed16a5f861554aab85ce22143eebcbddeb54df
ayeshanafis <ayeshanafis476@gmail.com>
2025-03-13 12:20:58 +0500

    numandmath

---
+++ 02_basics/numandmath.js
@@ -0,0 +1,39 @@
+const score= 400
+//console.log(score)
+
+const balance = new Number (100)
+//console.log(balance)
+
+//console.log(balance.toString().length)
+
+//console.log(balance.toFixed(2))
+
+const otherNumber= 123.894
+
+//console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'))
+
+//...................................Math..................................
+
+//console.log(Math)
+//mathod hai upar wala
+//console.log(Math.abs(-4))
+
+//console.log(Math.round(3.4))
+
+//console.log(Math.ceil(4.2))
+
+console.log(Math.random())
+console.log((Math.random() * 10 ) +1) 
+
+
+
+
+const min = 10
+const max = 30 
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
